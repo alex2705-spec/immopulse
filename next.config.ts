2743b8e9693cd854758c580020c6034aedde9151
Devdijimeo <112693073+Devdijimeo@users.noreply.github.com>
2026-03-20 23:38:16 +0100

    fix landing url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      { source: '/', destination: '/landing.html' },
+    ]
+  },
   async headers() {
     return [
       {
