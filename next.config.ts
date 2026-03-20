d07d52bfa876f7b2d6cb81a7b9776f32d11dcb26
Devdijimeo <112693073+Devdijimeo@users.noreply.github.com>
2026-03-20 23:02:56 +0100

    fix landing redirect

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+  async headers() {
     return [
-      { source: '/', destination: '/landing.html' },
+      {
+        source: '/carte',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; script-src * 'unsafe-inline' 'unsafe-eval'; worker-src * blob:;",
+          },
+        ],
+      },
     ]
   },
 };
